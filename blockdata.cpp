9cc49efdde094d8db99f19b920c08be1845348bc
Reverend Homer <mk.43.ecko@gmail.com>
2021-05-20 13:02:41 +0300

    blockdata: replace singleton class with namespace

--- blockdata.cpp
+++ blockdata.cpp
@@ -1,34 +1,36 @@
 /************************************************************************
-* Copyright (c) 2005-2007 tok@openlinux.org.uk                          *
-*                                                                       *
-* This software is provided as-is, without any express or implied       *
-* warranty. In no event will the authors be held liable for any         *
-* damages arising from the use of this software.                        *
-*                                                                       *
-* Permission is granted to anyone to use this software for any purpose, *
-* including commercial applications, and to alter it and redistribute   *
-* it freely, subject to the following restrictions:                     *
-*                                                                       *
-* 1. The origin of this software must not be misrepresented; you must   *
-* not claim that you wrote the original software. If you use this       *
-* software in a product, an acknowledgment in the product documentation *
-* would be appreciated but is not required.                             *
-*                                                                       *
-* 2. Altered source versions must be plainly marked as such, and must   *
-* not be misrepresented as being the original software.                 *
-*                                                                       *
-* 3. This notice may not be removed or altered from any source          *
-* distribution.                                                         *
-************************************************************************/
+ * Copyright (c) 2005-2007 tok@openlinux.org.uk                          *
+ *                                                                       *
+ * This software is provided as-is, without any express or implied       *
+ * warranty. In no event will the authors be held liable for any         *
+ * damages arising from the use of this software.                        *
+ *                                                                       *
+ * Permission is granted to anyone to use this software for any purpose, *
+ * including commercial applications, and to alter it and redistribute   *
+ * it freely, subject to the following restrictions:                     *
+ *                                                                       *
+ * 1. The origin of this software must not be misrepresented; you must   *
+ * not claim that you wrote the original software. If you use this       *
+ * software in a product, an acknowledgment in the product documentation *
+ * would be appreciated but is not required.                             *
+ *                                                                       *
+ * 2. Altered source versions must be plainly marked as such, and must   *
+ * not be misrepresented as being the original software.                 *
+ *                                                                       *
+ * 3. This notice may not be removed or altered from any source          *
+ * distribution.                                                         *
+ ************************************************************************/
 #include "blockdata.h"
 namespace OpenGTA {
-  float BlockData::slope_raw_data[numBlockTypes][numFaces][4][3] = {
+namespace BlockData {
+float slope_raw_data[numBlockTypes][numFaces][4][3] = {
 #include "slope1_data.h"
-  };
-  float BlockData::slope_tex_data[numBlockTypes][numFaces-1][4][2] = {
+};
+float slope_tex_data[numBlockTypes][numFaces - 1][4][2] = {
 #include "slope1_tcoords.h"
-  };
-  float BlockData::lid_normal_data[numBlockTypes][3] = { 
+};
+float lid_normal_data[numBlockTypes][3] = {
 #include "lid_normal_data.h"
-  };
-}
+};
+} // namespace BlockData
+} // namespace OpenGTA
--- blockdata.h
+++ blockdata.h
@@ -1,56 +1,43 @@
 /************************************************************************
-* Copyright (c) 2005-2007 tok@openlinux.org.uk                          *
-*                                                                       *
-* This software is provided as-is, without any express or implied       *
-* warranty. In no event will the authors be held liable for any         *
-* damages arising from the use of this software.                        *
-*                                                                       *
-* Permission is granted to anyone to use this software for any purpose, *
-* including commercial applications, and to alter it and redistribute   *
-* it freely, subject to the following restrictions:                     *
-*                                                                       *
-* 1. The origin of this software must not be misrepresented; you must   *
-* not claim that you wrote the original software. If you use this       *
-* software in a product, an acknowledgment in the product documentation *
-* would be appreciated but is not required.                             *
-*                                                                       *
-* 2. Altered source versions must be plainly marked as such, and must   *
-* not be misrepresented as being the original software.                 *
-*                                                                       *
-* 3. This notice may not be removed or altered from any source          *
-* distribution.                                                         *
-************************************************************************/
+ * Copyright (c) 2005-2007 tok@openlinux.org.uk                          *
+ *                                                                       *
+ * This software is provided as-is, without any express or implied       *
+ * warranty. In no event will the authors be held liable for any         *
+ * damages arising from the use of this software.                        *
+ *                                                                       *
+ * Permission is granted to anyone to use this software for any purpose, *
+ * including commercial applications, and to alter it and redistribute   *
+ * it freely, subject to the following restrictions:                     *
+ *                                                                       *
+ * 1. The origin of this software must not be misrepresented; you must   *
+ * not claim that you wrote the original software. If you use this       *
+ * software in a product, an acknowledgment in the product documentation *
+ * would be appreciated but is not required.                             *
+ *                                                                       *
+ * 2. Altered source versions must be plainly marked as such, and must   *
+ * not be misrepresented as being the original software.                 *
+ *                                                                       *
+ * 3. This notice may not be removed or altered from any source          *
+ * distribution.                                                         *
+ ************************************************************************/
 #ifndef OPENGTA_BLOCKDATA
 #define OPENGTA_BLOCKDATA
 #include <cstddef>
 
 namespace OpenGTA {
-  struct BlockData {
-    static const size_t numBlockTypes = 45;
-    static const size_t numFaces = 5;
+namespace BlockData {
 
-    static float slope_raw_data[numBlockTypes][numFaces][4][3];
-    static float slope_tex_data[numBlockTypes][numFaces-1][4][2];
-    static float lid_normal_data[numBlockTypes][3];
+constexpr size_t numBlockTypes = 45;
+constexpr size_t numFaces = 5;
 
-    static BlockData& Instance()
-    {
-      static BlockData instance;
-      return instance;
-    }
+extern float slope_raw_data[numBlockTypes][numFaces][4][3];
+extern float slope_tex_data[numBlockTypes][numFaces - 1][4][2];
+extern float lid_normal_data[numBlockTypes][3];
+} // namespace BlockData
+} // namespace OpenGTA
 
-    BlockData(const BlockData &copy) = delete;
-    BlockData& operator=(const BlockData &copy) = delete;
-  private:
-    BlockData() = default;
-    ~BlockData() = default;
-  };
-
-}
-
-#define SLOPE_RAW_DATA  OpenGTA::BlockData::Instance().slope_raw_data
-#define SLOPE_TEX_DATA  OpenGTA::BlockData::Instance().slope_tex_data
-#define LID_NORMAL_DATA OpenGTA::BlockData::Instance().lid_normal_data
-
+#define SLOPE_RAW_DATA OpenGTA::BlockData::slope_raw_data
+#define SLOPE_TEX_DATA OpenGTA::BlockData::slope_tex_data
+#define LID_NORMAL_DATA OpenGTA::BlockData::lid_normal_data
 
 #endif
